ff30a1981fceb99c6fe3d22d42c60aa38b44d9fc
Linn <linnskaghammar@yahoo.se>
2024-12-03 16:06:24 +0100

    movement

--- Assets/Scripts/Human.cs
+++ Assets/Scripts/Human.cs
@@ -1,57 +1,140 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
+using UnityEngine.U2D;
+using static UnityEditor.UIElements.ToolbarMenu;
 using static UnityEngine.GraphicsBuffer;
 
 public class Human : MonoBehaviour
 {
     
-    public int movementSpeed;
+    public float movementSpeed;
     private Vector3 target;
     GameObject humanInstance;
+    public GameObject Manager;
+    bool isIdle = false;
+    private SpriteRenderer spriteRenderer;
 
     // Start is called before the first frame update
     void Start()
     {
-        StartCoroutine(TargetSpawner());
+
+        //StartCoroutine(Idle());
+        StartCoroutine(Movement());
+        Manager = GameObject.Find("Manager");
+        spriteRenderer = GetComponent<SpriteRenderer>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (target != null)
-        {
-            Debug.Log("rör sig mot" + target);
-            transform.position = Vector3.MoveTowards(transform.position, target, Time.deltaTime * movementSpeed);
-        }
-        
+
+        transform.position = Vector3.MoveTowards(transform.position, target, Time.deltaTime * movementSpeed);
     }
+
+    //rörelse mot random position, byt pos efter X sek.
+    IEnumerator Movement()
+    {
+        while (gameObject != null)
+        {
+            if (isIdle == false)
+            {
+                target = new Vector3(Random.Range(-8, 8), Random.Range(-4, 4), 0);
+                movementSpeed = 1f;
+ 
+                //efter tiden gått ut, bli idle
+                yield return new WaitForSeconds(5);
+                //target = transform.position;
+                isIdle = true;
+            }
+    
+            else
+            {
+                movementSpeed = 0f;
+                yield return new WaitForSeconds(2);
+
+                isIdle = false;
+            }
+            yield return null;
+        }
+
+    }
+  
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        //50/50 risk att smittas
-        if (gameObject.tag == "Human" && humanInstance.tag == "Sick")
+        /*
+        Infektionsrisk vid kollision med sjuk -> Rull 0 - 99:
+        0 - 40 Sjuk
+        Annars: Inget händer
+        */
+        if (gameObject.tag == "Healthy")
         {
+            //VIT
+           spriteRenderer.color = new Color(1f, 1f, 1f, 1f);
+
             int Dice = Random.Range(0, 100);
-            if (Dice < 49)
+            if (Dice < 41)
             {
                 gameObject.tag = "Sick";
+                movementSpeed = 0.5f;
+                //RÖD
+                spriteRenderer.color = new Color(1f, 0f, 0f, 1f);
+                StartCoroutine(SicknessCounter());
+
             }
 
         }
+
+        /*
+        Timer:
+        När man varit sjuk en stund - konsekvenser -> Rull 0-99:
+        0-10 Död/dead
+        11-50 Frisk/healthy
+        51-99 Immun/immune
+        */
+        IEnumerator SicknessCounter()
+    {
+         //räkna inte counter, ändra bara tag
+
+            yield return new WaitForSeconds(2);
+            int Dice = Random.Range(0, 100);
+            //0 - 10 Död / dead
+            
+            if (Dice < 11)
+            {
+                gameObject.tag = "Dead";
+                movementSpeed = 0f;
+                //SVART
+                spriteRenderer.color = new Color(0f, 0f, 0f, 1f);
+
+            }
+
+            //11 - 50 Frisk / healthy
+            if (Dice > 10 && Dice < 51)
+            {
+                gameObject.tag = "Healthy";
+                //VIT
+                spriteRenderer.color = new Color(1f, 1f, 1f, 1f);
+
+            }
+            //51 - 99 Immun / immune
+            if (Dice > 50)
+            {
+                gameObject.tag = "Immune";
+                //GUL
+                spriteRenderer.color = new Color(1f, 1f, 0f, 1f);
+
+            }
+
+        }
+     
+
     }
 
-    //rörelse mot random position, byt pos efter x antal sek
-    IEnumerator TargetSpawner()
-    {
-         while (true)
-         {
-             if (gameObject != null)
-             {
-                 target = new Vector3(Random.Range(-8, 8), Random.Range(-4, 4), 0);
-                 yield return new WaitForSeconds(5);
-             }
-             yield return null;
-         }
-    }
 }
+
+
--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -1,10 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Spawner : MonoBehaviour
 {
     public int humanPopulation;
+    public int maxPop = 10;
+    public int healthyCounter;
     public int sickCounter;
     public int immuneCounter;
     public int deadCounter;
@@ -14,45 +17,77 @@ public class Spawner : MonoBehaviour
     void Start()
     {
         StartCoroutine(HumanSpawner());
+        //StartCoroutine(Human.GetComponent<Human>().SicknessCounter()); 
+        //spriteRenderer.color = new Color(1f, 1f, 1f, 1f);
+
+
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        healthyCounter = GameObject.FindGameObjectsWithTag("Healthy").Length;
+        sickCounter = GameObject.FindGameObjectsWithTag("Sick").Length;
+        immuneCounter = GameObject.FindGameObjectsWithTag("Immune").Length;
+        deadCounter = GameObject.FindGameObjectsWithTag("Dead").Length;
+        humanPopulation = healthyCounter + sickCounter + immuneCounter - deadCounter;
+
+
     }
 
     IEnumerator HumanSpawner()
     {
         while (true)
         {
-            if (humanPopulation < 50)
+            if (humanPopulation < maxPop)
             {
                 GameObject humanInstance = Instantiate(Human, new Vector3(Random.Range(-8, 8), Random.Range(-4, 4), 0), Quaternion.identity);
-                humanPopulation++;
+                humanInstance.tag = "Healthy";
+                //VIT
+                SpriteRenderer renderer = humanInstance.GetComponent<SpriteRenderer>();
+                renderer.color = new Color(1f, 1f, 1f, 1f);
                 
+
+                /* 
+                Infektionsrisk vid start -> Rull 0-99:
+                1 - 10 Infekterad/sick
+                11 - 90 Frisk/human/healthycounter
+                90 - 99 Immun/immune
+                */
                 int Dice = Random.Range(0, 100);
-                if (Dice  < 49)
+
+                if (Dice < 11)
                 {
                     humanInstance.tag = "Sick";
-                    sickCounter++;
-                } 
-                Debug.Log(humanPopulation);
+                    //RÖD
+                    renderer.color = new Color(1f, 0f, 0f, 1f);
+                    //bör starta sickness coroutine?
+                    //ändra movementspeed?
+
+                }
+                if (Dice > 89)
+                {
+                    humanInstance.tag = "Immune";
+                    //GUL
+                    renderer.color = new Color(1f, 1f, 0f, 1f);
+
+                }
+
+                if (humanPopulation == maxPop)
+                {
+                    Debug.Log("nu stannar spawn");
+                    break;
+
+                }
+
                 yield return new WaitForSeconds(0.1f);
             }
+            
             yield return null;
         }
     }
-    /*
-    public void HumanDeathTracker(int humanPopulation)
-    {
-        humanCounter--;
 
-        public int humanCounter;
-        public int sickCounter;
-        public int immuneCounter;
-        public int deadCounter;
-       
     }
-    */
-}
+
+
